d10bf7ce433ca4d4ea99b3ea09220354c41784e0
dpyxa <ail@worldticket.net>
2019-09-23 22:38:05 +0300

    test commit

--- app.py
+++ app.py
@@ -1,3 +1,11 @@
-print("this is just a test");
-print("this is just a test from a git");
-print("Анька писька");
+print("this is just a test")
+print("this is just a test from a git")
+print("Анька писька")
+
+myscore = 1000
+message = 'my score %s points'
+print(message % myscore)
+
+i1 = '%s: this is device for a furniture detection'
+i2 = 'leg'
+i3 = 'ankle'
