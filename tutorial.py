faaab86ae641a197278a297a57739f89c4069db0
Andrii Ilinov <ail@worldticket.net>
2020-08-06 15:54:47 +0300

    commit in super test branch

--- tutorial.py
+++ tutorial.py
@@ -1,3 +1,4 @@
 str = input("Enter your day: ")
-week = ["monday", "tuesday", "wednesday"]
+week = ["monday", "tuesday", "wednesday", "thursday", "friday", "saturday", "sunday"]
 print("Received input is : ", week[int(str)-1], "imput -1 is:", int(str)-1 )
+
