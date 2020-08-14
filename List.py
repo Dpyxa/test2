993542c48034b0b3061091a915a0e50f7f9c91c1
Dpyxa <38887769+dpyxa@users.noreply.github.com>
2020-08-14 09:43:15 +0300

    Trainig with lists №2

--- List.py
+++ List.py
@@ -1,2 +1,5 @@
-friends = ["drew", "mike", "kelly", "drew","drew"]
-print(friends.count("drew"))
+friends = ["drew", "mike", "kelly", "drew", "drew"]
+print(friends)
+friends.sort()
+print("\n \n \n")
+print(friends)
